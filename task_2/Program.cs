180fe2b5c6c88b78d6c595e82ebe55d76051a859
“Dmitriy” <colnce1-1@ya.ru>
2022-07-17 21:37:33 +0300

    add program find the third digit

---
+++ task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.Write($"Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number % 10;
+int result1 = number / 10 % 10;
+int result2 = number / 100 % 10;
+
+
+if (number >= 0 && number <= 99) {
+   Console.Write("Введите трехзначное число");
+} else {
+   if (number >= 100 && number <= 999){
+      Console.Write($"Заданное число: {number} третья цифра от числа {result}");
+   }
+   if (number >= 1000 && number <= 9999) {
+      Console.Write($"Заданное число: {number} третья цифра от числа {result1}");
+   } 
+   if (number >= 10000 && number <= 99999) {
+      Console.Write($"Заданное число: {number} третья цифра от числа {result2}");
+   } 
+}
