9c132183b1f36e0efa2f5a22a3f62ef09e2a6b35
“Dmitriy” <colnce1-1@ya.ru>
2022-07-17 21:13:52 +0300

    add program find the second number

---
+++ task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трех значное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number / 10;
+int ost = result % 10;
+
+if(number <= 99 || number >= 1000) {
+   Console.Write($"Вы вели не верное число {number}");
+} else {
+   Console.Write($"Вторая цифра нашего числа {number} равна {ost}");
+}
